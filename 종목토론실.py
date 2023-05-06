e182192a760ff03f47ec940d298eedcad9130031
Jun Eon Lee <126752298+joo-none@users.noreply.github.com>
2023-05-06 20:35:49 +0900

    Update 종목토론실.py

--- 종목토론실.py
+++ 종목토론실.py
@@ -172,7 +172,7 @@ name = krx[krx['Code'] == item_code]['Name'].iloc[0]
 
     
     
-st.text(f' 입력하신 종목은 <b>{name}({item_code})</b>입니다.', unsafe_allow_html=True)
+st.text(f' 입력하신 종목은 <b>{name}({item_code})</b>입니다.')
 # **{last_page}**개의 페이지를 출력합니다.'
 
 st.write(f" 네이버 종목토론실에서 해당 종목의 실시간 긍정 점수는 <h1><span style='color:{score_color};'>**{score}**</span></h1> 입니다.", unsafe_allow_html=True)
